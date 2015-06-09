72f25d33eec204dc98872f453b271b440ab7a342
ahhh <ahhh.db@gmail.com>
2015-06-09 01:21:27 -0700

    Create dir_recurser.py

---
+++ dir_recurser.py
@@ -0,0 +1,21 @@
+#/bin/python
+# Program for messing around with files and directories recursivly
+import os, sys
+from os.path import join, getsize
+
+def dir_recurse(target_directory):
+  for root, dirs, files in os.walk(target_directory):
+    print "Contents of directory: ", root
+    for file in files:
+      print str(os.path.getsize(os.path.join(target_directory, file)))+": "+file
+    for dir in dirs:
+      dir_recurse(os.path.join(target_directory, dir))
+    break
+  
+  
+if __name__ == '__main__':
+  try:
+    target_dir = sys.argv[1]
+    dir_recurse()
+  except IndexError:
+    print "python dir_recurser.py [/full/path/of/target/dir]"
